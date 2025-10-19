bdff84d990c861665241d7d76a97eb85037a9804
SydMontague <sydmontague@phoenix-staffel.de>
2025-10-19 13:08:37 +0200

    Tidy up AFS2

--- DSCSTools/AFS2.cpp
+++ DSCSTools/AFS2.cpp
@@ -1,122 +1,124 @@
 #include "include/AFS2.h"
 
-#include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
 #include <fstream>
+#include <iomanip>
+#include <iosfwd>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <vector>
-#include <stdint.h>
 
-namespace dscstools {
-	namespace afs2 {
-		constexpr auto AFS2_MAGIC_VALUE = '2SFA';
+namespace dscstools::afs2
+{
+    constexpr auto AFS2_MAGIC_VALUE = '2SFA';
 
-		struct AFS2Header {
-			uint32_t magic;
-			uint32_t flags;
-			uint32_t numFiles;
-			int32_t blockSize;
-		};
+    struct AFS2Header
+    {
+        uint32_t magic;
+        uint32_t flags;
+        uint32_t numFiles;
+        int32_t blockSize;
+    };
 
-		void extractAFS2(const std::filesystem::path source, const std::filesystem::path target) {
-			if (std::filesystem::exists(target) && !std::filesystem::is_directory(target))
-				throw std::invalid_argument("Error: Target path exists and is not a directory, aborting.");
-			if (!std::filesystem::is_regular_file(source))
-				throw std::invalid_argument("Error: Source path doesn't point to a file, aborting.");
+    void extractAFS2(const std::filesystem::path& source, const std::filesystem::path& target)
+    {
+        if (std::filesystem::exists(target) && !std::filesystem::is_directory(target))
+            throw std::invalid_argument("Error: Target path exists and is not a directory, aborting.");
+        if (!std::filesystem::is_regular_file(source))
+            throw std::invalid_argument("Error: Source path doesn't point to a file, aborting.");
 
-			std::ifstream input(source, std::ios::in | std::ios::binary);
+        std::ifstream input(source, std::ios::in | std::ios::binary);
 
-			AFS2Header header;
-			input.read((char*)&header, 0x10);
+        AFS2Header header{};
+        input.read(reinterpret_cast<char*>(&header), 0x10);
 
-			if (header.magic != AFS2_MAGIC_VALUE) 
-				throw std::invalid_argument("Error: not an AFS2 file. Value: ");
+        if (header.magic != AFS2_MAGIC_VALUE) throw std::invalid_argument("Error: not an AFS2 file. Value: ");
 
-			auto fileIds = std::make_unique<uint16_t[]>(header.numFiles);
-			input.read(reinterpret_cast<char*>(fileIds.get()), header.numFiles * 2);
+        std::vector<uint16_t> fileIds(header.numFiles);
+        input.read(reinterpret_cast<char*>(fileIds.data()), header.numFiles * 2L);
 
-			auto offsets = std::make_unique<uint32_t[]>(header.numFiles + 1LL);
-			input.read(reinterpret_cast<char*>(offsets.get()), (header.numFiles + 1) * 4);
+        std::vector<uint32_t> offsets(header.numFiles + 1);
+        input.read(reinterpret_cast<char*>(offsets.data()), (header.numFiles + 1) * 4L);
 
-			if (input.tellg() < header.blockSize)
-				input.seekg(header.blockSize);
-			if (input.tellg() != offsets[0])
-				throw std::invalid_argument("AFS2: Didn't reach expected end of header.");
+        if (input.tellg() < header.blockSize) input.seekg(header.blockSize);
+        if (input.tellg() != offsets[0]) throw std::invalid_argument("AFS2: Didn't reach expected end of header.");
 
-			if(target.has_parent_path())
-				std::filesystem::create_directories(target);
+        if (target.has_parent_path()) std::filesystem::create_directories(target);
 
-			for (size_t i = 0; i < header.numFiles; i++) {
-				input.seekg((std::streampos) ((uint32_t)input.tellg() + header.blockSize - 1) & -header.blockSize);
-				uint32_t size = offsets[i + 1] - (uint32_t)input.tellg();
+        for (size_t i = 0; i < header.numFiles; i++)
+        {
+            input.seekg((static_cast<uint32_t>(input.tellg()) + header.blockSize - 1) & -header.blockSize); // NOLINT
+            uint32_t size = offsets[i + 1] - (uint32_t)input.tellg();
 
-				auto data = std::make_unique<char[]>(size);
-				input.read(data.get(), size);
+            std::vector<char> data(size);
+            input.read(data.data(), size);
 
-				std::stringstream sstream;
-				sstream << std::setw(6) << std::setfill('0') << std::hex << i << ".hca";
+            std::stringstream sstream;
+            sstream << std::setw(6) << std::setfill('0') << std::hex << i << ".hca";
 
-				std::filesystem::path path(target / sstream.str());
-				std::ofstream output(path, std::ios::out | std::ios::binary);
+            std::filesystem::path path(target / sstream.str());
+            std::ofstream output(path, std::ios::out | std::ios::binary);
 
-				output.write(data.get(), size);
-			}
-		}
+            output.write(data.data(), size);
+        }
+    }
 
-		void packAFS2(const std::filesystem::path source, const std::filesystem::path target) {
-			if (!std::filesystem::is_directory(source)) 
-				throw std::invalid_argument("Error: source path is not a directory.");
+    void packAFS2(const std::filesystem::path& source, const std::filesystem::path& target)
+    {
+        if (!std::filesystem::is_directory(source))
+            throw std::invalid_argument("Error: source path is not a directory.");
 
-			if (!std::filesystem::exists(target)) {
-				if (target.has_parent_path())
-					std::filesystem::create_directories(target.parent_path());
-			}
-			else if (!std::filesystem::is_regular_file(target))
-				throw std::invalid_argument("Error: target path already exists and is not a file.");
+        if (!std::filesystem::exists(target))
+        {
+            if (target.has_parent_path()) std::filesystem::create_directories(target.parent_path());
+        }
+        else if (!std::filesystem::is_regular_file(target))
+            throw std::invalid_argument("Error: target path already exists and is not a file.");
 
-			std::ofstream output(target, std::ios::out | std::ios::binary);
+        std::ofstream output(target, std::ios::out | std::ios::binary);
 
-			std::vector<std::filesystem::path> files;
+        std::vector<std::filesystem::path> files;
 
-			for (auto i : std::filesystem::directory_iterator(source))
-				if (std::filesystem::is_regular_file(i))
-					files.push_back(i);
+        for (const auto& i : std::filesystem::directory_iterator(source))
+            if (std::filesystem::is_regular_file(i)) files.push_back(i);
 
-			AFS2Header header;
-			header.magic = AFS2_MAGIC_VALUE;
-			header.flags = 0x00020402;
-			header.numFiles = (uint32_t)files.size();
-			header.blockSize = 0x20;
+        AFS2Header header{};
+        header.magic     = AFS2_MAGIC_VALUE;
+        header.flags     = 0x00020402;
+        header.numFiles  = (uint32_t)files.size();
+        header.blockSize = 0x20;
 
-			output.write((char*)&header, 0x10);
+        output.write(reinterpret_cast<char*>(&header), 0x10);
 
-			auto id = std::make_unique<uint16_t[]>(header.numFiles);
-			auto offsets = std::make_unique<uint32_t[]>(header.numFiles + 1);
+        std::vector<uint16_t> id(header.numFiles);
+        std::vector<uint32_t> offsets(header.numFiles + 1);
 
-			offsets[0] = 0x10 + header.numFiles * 0x06 + 4;
-			if (offsets[0] < header.blockSize)
-				offsets[0] = header.blockSize;
+        offsets[0] = 0x10 + header.numFiles * 0x06 + 4;
+        offsets[0] = std::max(offsets[0], static_cast<uint32_t>(header.blockSize));
 
-			for (size_t i = 0; i < files.size(); i++) {
-				output.seekp((offsets[i] + header.blockSize - 1) & -header.blockSize);
+        for (size_t i = 0; i < files.size(); i++)
+        {
+            output.seekp((offsets[i] + header.blockSize - 1) & -header.blockSize); // NOLINT
 
-				std::ifstream input(files[i], std::ios::in | std::ios::binary);
-				input.seekg(0, std::ios::end);
-				std::streamoff length = input.tellg();
-				input.seekg(0, std::ios::beg);
+            std::ifstream input(files[i], std::ios::in | std::ios::binary);
+            input.seekg(0, std::ios::end);
+            std::streamoff length = input.tellg();
+            input.seekg(0, std::ios::beg);
 
-				auto buffer = std::make_unique<char[]>(length);
-				input.read(buffer.get(), length);
-				output.write(buffer.get(), length);
+            std::vector<char> buffer(length);
+            input.read(buffer.data(), length);
+            output.write(buffer.data(), length);
 
-				id[i] = (uint16_t)i;
-				offsets[i + 1] = (uint32_t)output.tellp();
-			}
+            id[i]          = (uint16_t)i;
+            offsets[i + 1] = (uint32_t)output.tellp();
+        }
 
-			output.seekp(0x10);
-			output.write(reinterpret_cast<char*>(id.get()), header.numFiles * 2);
-			//output.seekp((header.numFiles % 2) * 2, std::ios_base::cur);
-			output.write(reinterpret_cast<char*>(offsets.get()), (header.numFiles + 1) * 4);
-		}
-	}
-}
+        output.seekp(0x10);
+        output.write(reinterpret_cast<char*>(id.data()), header.numFiles * 2L);
+        output.write(reinterpret_cast<char*>(offsets.data()), (header.numFiles + 1) * 4L);
+    }
+} // namespace dscstools::afs2
